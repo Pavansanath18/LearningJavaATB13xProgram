fba1dda6e02b79ce7b005c972dd6f10ddb7fd444
pavan <pavansanath51@gmail.com>
2025-06-10 18:07:10 +0530

    basic program

---
+++ src/ex_02_java_Basic/Lab_ex_NineTable.java
@@ -0,0 +1,30 @@
+package ex_02_java_Basic;
+
+public class Lab_ex_NineTable {
+    public static void main(String[] args){
+    int a = 9;
+        System.out.printf("9X1= %d\n",a*1);
+        System.out.printf("9X2= %d\n",a*2);
+        System.out.printf("9X3= %d\n",a*3);
+        System.out.printf("9X4= %d\n",a*4);
+        System.out.printf("9X5= %d\n",a*5);
+        System.out.printf("9X6= %d\n",a*6);
+        System.out.printf("9X7= %d\n",a*7);
+        System.out.printf("9X8= %d\n",a*8);
+        System.out.printf("9X9= %d\n",a*9);
+        System.out.printf("9X10= %d\n",a*10);
+
+// another method to write table 9
+        System.out.printf("%d\n", 9 * 1);
+        System.out.printf("%d\n", 9 * 2);
+        System.out.printf("%d\n", 9 * 3);
+        System.out.printf("%d\n", 9 * 4);
+        System.out.printf("%d\n", 9 * 5);
+        System.out.printf("%d\n", 9 * 6);
+        System.out.printf("%d\n", 9 * 7);
+        System.out.printf("%d\n", 9 * 8);
+        System.out.printf("%d\n", 9 * 9);
+        System.out.printf("%d\n", 9 * 10);
+
+    }
+}
