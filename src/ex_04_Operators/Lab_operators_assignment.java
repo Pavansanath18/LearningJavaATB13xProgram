096dd9b9f77d389538edb75b891dec294fe9a821
pavan <pavansanath51@gmail.com>
2025-06-11 20:01:46 +0530

    basic program

---
+++ src/ex_04_Operators/Lab_operators_assignment.java
@@ -0,0 +1,15 @@
+package ex_04_Operators;
+
+public class Lab_operators_assignment {
+
+    public static void main(String[] args) {
+        int age = 65;
+        // int -> data type
+        // age -> identifier
+        // = -> Assignment operator
+        // 65 -> Literal
+
+        // RHV to the LH
+    }
+}
+
